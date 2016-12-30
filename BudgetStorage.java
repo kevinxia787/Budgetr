b3094c3d9a2150d9542683bd2f0ac27df8e2d5cf
Kevin Xia <kevinxia787@gmail.com>
2016-12-29 20:25:28 -0800

    added methods to get all spending categories

--- BudgetStorage.java
+++ BudgetStorage.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class BudgetStorage {
 	
@@ -36,9 +37,9 @@ public class BudgetStorage {
 			return this.category;
 		}
 	}
-	
 	private final int SIZE = 29;
-	Node [] storage = new Node[SIZE];
+	Node [] storage = new Node[SIZE]; 
+	// Set initial values of percent/spending to 0/0.0
 	
 	
 	//Hash Function Here
@@ -86,27 +87,62 @@ public class BudgetStorage {
 	public int searchPercent(String c) {
 		return (storage[hash(c, SIZE)].percentOfSalary);
 	}
+	//Iterates through the hash table and returns an ArrayList of all spending
+	public ArrayList<Double> getAllSpending() {
+		ArrayList<Double> spending = new ArrayList<Double>();
+		for (int i = 0; i < SIZE; ++i) {
+			if (storage[i] != null) {
+				spending.add(storage[i].spending);
+			}
+		}
+		return spending;
+	}
+	//Iterates through the ArrayList and gets the sum 
+	public double totalSpending(ArrayList<Double> c) {
+		double total = 0.0;
+		for (int i = 0; i < c.size(); ++i) {
+			total += c.get(i);
+		}
+		return total;
+	}
 	
 	public static void main(String[] args) {
 		
 		BudgetStorage B = new BudgetStorage();
 		B.insert("loans", 447.00);
 		B.insert("groceries", 225.00);
-		
+		B.insert("rent", 1250.00);
+		B.insert("transport", 100.00);
 		System.out.println("Test 01:  Should print out\n" + 447.00);
 		System.out.println(B.searchSpending("loans"));
 		
+		System.out.println("--------------------------");
+		
 		System.out.println("Test 02:  Should print out\n" + 225.00);
 		System.out.println(B.searchSpending("groceries"));
+		
+		System.out.println("--------------------------");
+		
+		System.out.println("Test 03:  Should print out\n[447.0, 1250.0, 100.0, 225.0]");
+		System.out.println(B.getAllSpending());
+		System.out.println("--------------------------");
+		
+		System.out.println("Test 04:  Should print out\n2022.0");
+		System.out.println(B.totalSpending(B.getAllSpending()));
+		
+		System.out.println("--------------------------");
 		
 		BudgetStorage C = new BudgetStorage();
 		C.insert("groceries", 10);
 		C.insert("utilities", 25);
 		
-		System.out.println("Test 01:  Should print out\n" + 10);
+		
+		System.out.println("Test 05:  Should print out\n" + 10);
 		System.out.println(C.searchPercent("groceries"));
 		
-		System.out.println("Test 02:  Should print out\n" + 25);
+		System.out.println("--------------------------");
+		
+		System.out.println("Test 06:  Should print out\n" + 25);
 		System.out.println(C.searchPercent("utilities"));
 		
 	}
