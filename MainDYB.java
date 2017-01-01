3dd45e07e4df4d05f4534eef8d53745d93d471a2
Kevin Xia <kevinxia787@gmail.com>
2016-12-31 22:52:22 -0800

    Completed percent option in main

--- MainDYB.java
+++ MainDYB.java
@@ -20,8 +20,15 @@ public class MainDYB {
 		BudgetStorage B = new BudgetStorage();
 		double salary = 0;
 		double principal; double interest; int period; double loanPayment;
+		double rent; double rentPercent; double groceries; double groceriesPercent;
+		double utilities; double utilitiesPercent; double transportation; double transportationPercent;
+		double recreation; double recreationPercent; double savings; double savingsPercent;
+		double emergency; double emergencyPercent; double percentRemaining; double fundsRemaining;
+		
 		
 		int choice = userInput.nextInt();
+		
+		//User enters percent of desired spending per category
 		if (choice == 1) {
 			System.out.println("Please enter your salary: ");
 			salary += userInput.nextDouble();
@@ -31,6 +38,8 @@ public class MainDYB {
 			System.out.println("--------------------");
 			System.out.println("Please choose between 1 or 2. ");
 			int loanChoice = userInput.nextInt();
+			
+			// User has a loan he/she would like to pay off in ____ months
 			if (loanChoice == 1) {
 				System.out.println("Please enter your principal: ");
 				principal = userInput.nextDouble();
@@ -46,17 +55,90 @@ public class MainDYB {
 				System.out.println("2. Annually");
 				System.out.println("--------------------");
 				int rateChoice = userInput.nextInt();
+				
+				// Loan interest rate is monthly
 				if (rateChoice == 1) {
 					loanPayment = BudgetMath.loanPayment(principal, period, interest, false);
 				}
+				// Loan interest rate is annually
 				else { loanPayment = BudgetMath.loanPayment(principal, period, interest, true); }
-				B.insert("Loans", loanPayment);
+				B.insert("Loans", loanPayment, BudgetMath.getPercentSpending(salary, loanPayment));
 			}
-		}
-		else if (choice == 2) {
+			
+			// User does not have loans. Continue to inquire desired percentages
+			System.out.println("Please enter desired percentage spending for Rent: ");
+			rentPercent = userInput.nextInt();
+			B.insert("Rent", BudgetMath.getPercentValue(salary, rentPercent), rentPercent);
+			System.out.println("--------------------");
+			System.out.println("Please enter desired percentage spending for Utilities: ");
+			utilitiesPercent = userInput.nextInt();
+			B.insert("Utilities", BudgetMath.getPercentValue(salary, utilitiesPercent), utilitiesPercent);
+			System.out.println("--------------------");
+			System.out.println("Please enter desired percentage spending for Groceries: "); 
+			groceriesPercent = userInput.nextInt();
+			B.insert("Groceries", BudgetMath.getPercentValue(salary, groceriesPercent), groceriesPercent);
+			System.out.println("--------------------");
+			System.out.println("Please enter desired percentage spending for Transportation: ");
+			transportationPercent = userInput.nextInt();
+			B.insert("Transportation", BudgetMath.getPercentValue(salary, transportationPercent), transportationPercent);
+			System.out.println("--------------------");
+			System.out.println("DisplayYourBudget will now calculate the remaining funds for the month, and split funds into Recreation/Savings/Emergency.");
+			percentRemaining = BudgetMath.getRemainderPercent(B.totalPercent(B.getAllPercents()));
+			System.out.println("Remaining percent: " + percentRemaining);
+			fundsRemaining = BudgetMath.getPercentValue(salary, percentRemaining);
+			System.out.println("Remaining funds: " + fundsRemaining);
+			System.out.println("--------------------");
+			savingsPercent = BudgetMath.getSRPercent(percentRemaining);
+			savings = BudgetMath.getPercentValue(salary, savingsPercent);
+			System.out.println("How much you should allocate towards Savings: " + savings);
+			recreationPercent = BudgetMath.getSRPercent(percentRemaining);
+			recreation = BudgetMath.getPercentValue(salary, recreationPercent);
+			System.out.println("How much you should spend on Recreation: " + recreation);
+			emergencyPercent = BudgetMath.getEmergencyPercent(percentRemaining);
+			emergency = BudgetMath.getPercentValue(salary, emergencyPercent);
+			System.out.println("How much you should allocate towards Emergency Fund: " + emergency);
+			System.out.println("--------------------");
+			B.insert("Savings", savings, savingsPercent);
+			B.insert("Recreation", recreation, recreationPercent);
+			B.insert("Emergency", emergency, emergencyPercent);
+			B.getChart();
 			
 		}
 		else {
+			System.out.println("Please enter your salary: ");
+			salary += userInput.nextDouble();
+			System.out.println("--------------------");
+			System.out.println("1. I have loans.");
+			System.out.println("2. I do not have loans");
+			System.out.println("--------------------");
+			System.out.println("Please choose between 1 or 2. ");
+			int loanChoice = userInput.nextInt();
+			
+			// User has a loan he/she would like to pay off in ____ months
+			if (loanChoice == 1) {
+				System.out.println("Please enter your principal: ");
+				principal = userInput.nextDouble();
+				System.out.println("--------------------");
+				System.out.println("Please enter your interest rate in decimal form: ");
+				interest = userInput.nextDouble();
+				System.out.println("--------------------");
+				System.out.println("Please enter the period in months: ");
+				period = userInput.nextInt();
+				System.out.println("--------------------");
+				System.out.println("Is the interest monthly or annually?");
+				System.out.println("1. Monthly");
+				System.out.println("2. Annually");
+				System.out.println("--------------------");
+				int rateChoice = userInput.nextInt();
+				
+				// Loan interest rate is monthly
+				if (rateChoice == 1) {
+					loanPayment = BudgetMath.loanPayment(principal, period, interest, false);
+				}
+				// Loan interest rate is annually
+				else { loanPayment = BudgetMath.loanPayment(principal, period, interest, true); }
+				B.insert("Loans", loanPayment, BudgetMath.getPercentSpending(salary, loanPayment));
+			}
 			
 		}
 	}
